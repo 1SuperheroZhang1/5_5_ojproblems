0c03593741c3986d51043cc7ae1ef2d08c9a9952
zhangcy <2304417358@qq.com>
2023-01-13 20:58:05 +0800

    C语言督学营 - 5.5 oj题目

---
+++ cmake-build-debug/CMakeFiles/5_5_ojproblems.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_5_ojproblems.exe"
+  "5_5_ojproblems.exe.manifest"
+  "5_5_ojproblems.pdb"
+  "CMakeFiles/5_5_ojproblems.dir/main.c.obj"
+  "lib5_5_ojproblems.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/5_5_ojproblems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
